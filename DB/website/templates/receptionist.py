fdc8fbe74668c3e6b2063efa1826020668d62c15
JohnnyCanuck328 <91069350+JohnnyCanuck328@users.noreply.github.com>
2022-04-13 21:14:12 -0400

    added some methods for interacting with databases in receptionist. The method testInsertion needs work. To get working, line 15 will need to be changed. Additionally lines 79 and 80 should be uncommented the first time run to create the nessisary test database

--- DB/website/templates/receptionist.py
+++ DB/website/templates/receptionist.py
@@ -1,7 +1,86 @@
-from flask import Blueprint, request, flash
+from flask import Blueprint, request, flash, redirect, url_for, request
 import js2py
 
+import mysql.connector
+from mysql.connector import Error
+import pandas as pd
+
 receptionist = Blueprint('receptionist', __name__)
+
+
+#-----------------------------------------------------------------------------
+#Database stuff
+
+def testInsertion(fName, lName):
+    connection = create_db_connection("localhost", "root", "1qaz@WSX", "school")
+    mySql_insert_query = """
+    INSERT INTO teacher VALUES
+    (%s,  %s, %s, %s, %s, %s, %s, %s);
+    """
+    record = (8, fName, lName, "ENG", None, '1985-04-20', 123456, '+491774553676')
+    cursor = connection.cursor()
+    cursor.execute(mySql_insert_query, record)
+
+def create_server_connection(host_name, user_name, user_password):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password
+        )
+        print("MySQL Database connection successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+    return connection
+
+#connection = create_db_connection(hostName "localhost", username "root", password, tableName)
+def create_db_connection(host_name, user_name, user_password, db_name):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password,
+            database=db_name
+        )
+        print("MySQL Database connection successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+    return connection
+
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+
+#----USED TO CREATE TEST DATABASE
+create_teacher_table = """
+CREATE TABLE teacher (
+  teacher_id INT PRIMARY KEY,
+  first_name VARCHAR(40) NOT NULL,
+  last_name VARCHAR(40) NOT NULL,
+  language_1 VARCHAR(3) NOT NULL,
+  language_2 VARCHAR(3),
+  dob DATE,
+  tax_id INT UNIQUE,
+  phone_no VARCHAR(20)
+  );
+ """
+ 
+#uncomment first time running to create database school
+#create_database_query = "CREATE DATABASE school"
+#create_database(connection, create_database_query)
+ 
+#-----------------------------------------------------------------------------
+
 
 @receptionist.route('/receptionist/selection', methods=['GET', 'POST'])
 def selection():
@@ -36,13 +115,16 @@ def selection():
 
 @receptionist.route('/receptionist/patient', methods=['GET', 'POST'])
 def patient():
-    data = request.form
-    print(data)
+    first_name = request.form.get('fName')
+    last_name = request.form.get('lName')
+    if request.method == "POST":
+        testInsertion(first_name, last_name)
+    
     return '''
         <h2>Add and Update Patient Information</h2>
 
         <!-- change GET to POST (post works with servers) emailaddress, dateofbirth, age-->
-        <form name="repPat" form action="test.php" onsubmit="return validateForm()" method="POST">
+        <form name="repPat" form action="" onsubmit="return validateForm()" method="POST">
         <label for="ssn">SSN:</label>
         <input type="text" id="ssn" name="ssn" value=""><br><br>
         <label for="fName">First Name:</label>
