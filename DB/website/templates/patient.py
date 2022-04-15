d6e63005a01c824fe9ef7ef7e8fb6bf28b18c622
Lanhui-Chen <lchen245@uottawa.ca>
2022-04-15 16:21:17 -0400

    login test

--- DB/website/templates/patient.py
+++ DB/website/templates/patient.py
@@ -1,7 +1,51 @@
-from flask import Blueprint, render_template, request, flash
+from unicodedata import category
+from flask import Blueprint, request,render_template, flash, redirect, url_for, request
+
+import mysql.connector
+from mysql.connector import Error
+import pandas as pd
 
 patient = Blueprint('patient', __name__)
 
+# connection information
+hostName = "localhost"
+databaseName = "project"
+userName = "root"
+passwordString = "1qaz@WSX"
+
+#ID testing
+patientID = ""
+uid = "123456"
+
+def idCheck(id):
+    mydb = mysql.connector.connect(
+        host = hostName,
+        database=databaseName,
+        user=userName,
+        password=passwordString
+    )
+    mycursor=mydb.cursor()
+    mycursor.execute("SELECT USERID FROM USER_")
+    userid = mycursor.fetchall()
+
+    for uerid in userid:
+        if id == userid:
+            return True
+    return False
+
+@patient.route('/patient/login', methods=['GET', 'POST'])
+def login():
+
+    id = request.form.get('pID')
+    if request.method == "POST":
+        if idCheck(id):
+                flash('Success', category='success')
+                login = True
+                return redirect(url_for('patient.selection'))
+        else:
+            flash('Invalid patient ID, try again.', category='error')
+         
+    return render_template("patientLogin.html")
 
 @patient.route('/patient/selection', methods=['GET', 'POST'])
 def selection():
@@ -24,11 +68,8 @@ def selection():
 @patient.route('/patient/appointment', methods=['GET', 'POST'])
 def appointment():
     data = request.form
-    somedata = (("Rolf", "Software Engineer", "45000"),
-               ("sponge", "Hardware Engineer", "120000"),
-                ("Bob", "Mechanical Engineer", "40000"))
 
-    return render_template("patientAppointment.html", data = somedata)
+    return render_template("patientAppointment.html")
 
 
 @patient.route('/patient/record', methods=['GET', 'POST'])
